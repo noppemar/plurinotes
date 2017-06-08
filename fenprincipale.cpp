be9effd8cb17227000a44e93e22fc4381a0d7401
ymaillet <yannmailletpro@gmail.com>
2017-06-08 19:56:11 +0200

    Update fenprincipale.cpp

--- fenprincipale.cpp
+++ fenprincipale.cpp
@@ -1,34 +1,60 @@
 #include "FenPrincipale.h"
-#include "FenetreCreerNotes.h"
+#include "FenetreCreerArticle.h"
+#include "FenetreCreerTache.h"
 
 
 
 FenetrePrincipale::FenetrePrincipale()
 {
+    QMdiArea *zoneCentrale = new QMdiArea;
 
-    QMenu *menuFichier = menuBar()->addMenu("&Fichier");
+    QMenu *menuFichier = menuBar()->addMenu("&Fichier");                          // creation du menu fichier
 
-    QAction *actionQuitter = new QAction("&Quitter", this);
-    menuFichier->addAction(actionQuitter);
-    connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+        QMenu *actionNouvelleNote = menuFichier->addMenu("Nouvelle Note");       //creation  sous menu permettant de creer une nouvelle note :
 
-    m_creer_note = new QPushButton("Creer une nouvelle note", this);      // creation du bouton
-    m_creer_note2 = new QPushButton("It's a trap", this);
-    QObject::connect(m_creer_note, SIGNAL(clicked()), this, SLOT(afficherCreerNotes()));  //connection au slot creerNotes
+            QAction *actionNouvelleTache = new QAction("Nouvelle Tache", this);    //  tache
+                actionNouvelleNote->addAction(actionNouvelleTache);
+                connect(actionNouvelleTache, SIGNAL(triggered()), this, SLOT(afficherCreerTache()));
+
+            QAction *actionNouvelArticle = new QAction("Nouvel Article", this);    //  Article
+                 actionNouvelleNote->addAction(actionNouvelArticle);
+                 connect(actionNouvelArticle, SIGNAL(triggered()), this, SLOT(afficherCreerArticle()));
 
 
-    QWidget *zoneCentrale = new QWidget;    //permet d'afficher notre layout dans la fenetre principale
+         QMenu *actionEditerNote = menuFichier->addMenu("Editer Note");       //creation  sous menu permettant d'éditer une note:
 
-    QGridLayout *layout = new QGridLayout;
-    layout->addWidget(m_creer_note2, 0, 0, 5, 2);
-    layout->addWidget(m_creer_note, 0, 10, 5, 2);
+             QAction *actionEditerTache = new QAction("Editer Tache", this);    //  tache
+                  actionEditerNote->addAction(actionEditerTache);
+                  connect(actionEditerTache, SIGNAL(triggered()), this, SLOT(afficherEditerTache()));
 
-    zoneCentrale->setLayout(layout);
+             QAction *actionEditerArticle = new QAction("Editer Article", this);    //  Article
+                  actionEditerNote->addAction(actionEditerArticle);
+                  connect(actionEditerArticle, SIGNAL(triggered()), this, SLOT(afficherEditerArticle()));
+
+         QAction *actionQuitter = new QAction("&Quitter", this);
+             menuFichier->addAction(actionQuitter);
+             connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+
     setCentralWidget(zoneCentrale);
 }
 
 
-void FenetrePrincipale::afficherCreerNotes() {                    // permet d'afficher une fenetre de type creerNotes au clic
-    FenetreCreerNotes *fenetre = new FenetreCreerNotes;
-    fenetre->show();
+void FenetrePrincipale::afficherCreerTache() {
+    FenetreCreerTache *fenetreCreerTache = new FenetreCreerTache(this);
+    setCentralWidget(fenetreCreerTache);
+}
+
+void FenetrePrincipale::afficherCreerArticle() {
+    FenetreCreerArticle *fenetreCreerArticle = new FenetreCreerArticle(this);
+    setCentralWidget(fenetreCreerArticle);
+}
+
+
+void FenetrePrincipale::afficherEditerTache() {
+
+}
+
+void FenetrePrincipale::afficherEditerArticle() {
+
 }
