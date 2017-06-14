fde148c866fa018f5de6eb97574394371fdb8e1d
noppemar <marie.d.noppe@gmail.com>
2017-06-14 22:34:22 +0200

    affichage et update ok

--- fenprincipale.cpp
+++ fenprincipale.cpp
@@ -12,73 +12,75 @@ FenetrePrincipale::FenetrePrincipale()
 
     QMenu *menuFichier = menuBar()->addMenu("&Fichier");                          // creation du menu fichier
 
-        QMenu *actionNouvelleNote = menuFichier->addMenu("Nouvelle Note");       //creation  sous menu permettant de creer une nouvelle note :
+    QMenu *actionNouvelleNote = menuFichier->addMenu("Nouvelle Note");       //creation  sous menu permettant de creer une nouvelle note :
 
-            QAction *actionNouvelleTache = new QAction("Nouvelle Tache", this);    //  tache
-                actionNouvelleNote->addAction(actionNouvelleTache);
-                connect(actionNouvelleTache, SIGNAL(triggered()), this, SLOT(afficherCreerTache()));
+    QAction *actionNouvelleTache = new QAction("Nouvelle Tache", this);    //  tache
+    actionNouvelleNote->addAction(actionNouvelleTache);
+    connect(actionNouvelleTache, SIGNAL(triggered()), this, SLOT(afficherCreerTache()));
 
-            QAction *actionNouvelArticle = new QAction("Nouvel Article", this);    //  Article
-                 actionNouvelleNote->addAction(actionNouvelArticle);
-                 connect(actionNouvelArticle, SIGNAL(triggered()), this, SLOT(afficherCreerArticle()));
+    QAction *actionNouvelArticle = new QAction("Nouvel Article", this);    //  Article
+    actionNouvelleNote->addAction(actionNouvelArticle);
+    connect(actionNouvelArticle, SIGNAL(triggered()), this, SLOT(afficherCreerArticle()));
 
-            QAction *actionNouvelMulti = new QAction("Nouveau Multimedia", this);    //  Multimedia  add 13/06
-                      actionNouvelleNote->addAction(actionNouvelMulti);
-                      connect(actionNouvelMulti, SIGNAL(triggered()), this, SLOT(afficherCreerMulti()));
+    QAction *actionNouvelMulti = new QAction("Nouveau Multimedia", this);    //  Multimedia  add 13/06
+    actionNouvelleNote->addAction(actionNouvelMulti);
+    connect(actionNouvelMulti, SIGNAL(triggered()), this, SLOT(afficherCreerMulti()));
 
-       QMenu *actionNewRelation = menuFichier->addMenu("Nouvelle Relation");
-                      QAction *actionNouvelleRelation = new QAction("Nouvelle Relation", this);
-                                actionNewRelation->addAction(actionNouvelleRelation);
-                                connect(actionNouvelleRelation, SIGNAL(triggered()), this, SLOT(afficherCreerRelation()));
+    QMenu *actionNewRelation = menuFichier->addMenu("Nouvelle Relation");
+    QAction *actionNouvelleRelation = new QAction("Nouvelle Relation", this);
+    actionNewRelation->addAction(actionNouvelleRelation);
+    connect(actionNouvelleRelation, SIGNAL(triggered()), this, SLOT(afficherCreerRelation()));
 
-         QMenu *actionEditerNote = menuFichier->addMenu("Editer Note");       //creation  sous menu permettant d'éditer une note:
 
-             QAction *actionEditerTache = new QAction("Editer Tache", this);    //  tache
-                  actionEditerNote->addAction(actionEditerTache);
-                  connect(actionEditerTache, SIGNAL(triggered()), this, SLOT(afficherEditerTache()));
-
-             QAction *actionEditerArticle = new QAction("Editer Article", this);    //  Article
-                  actionEditerNote->addAction(actionEditerArticle);
-                  connect(actionEditerArticle, SIGNAL(triggered()), this, SLOT(afficherEditerArticle()));
-
-             QAction *actionEditerMulti = new QAction("Editer Multimedia", this);    //  Multimedia
-                       actionEditerNote->addAction(actionEditerMulti);
-                       connect(actionEditerMulti, SIGNAL(triggered()), this, SLOT(afficherEditerMulti()));
-
-         QAction *actionQuitter = new QAction("&Quitter", this);
-             menuFichier->addAction(actionQuitter);
-             connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+    QAction *actionQuitter = new QAction("&Quitter", this);
+    menuFichier->addAction(actionQuitter);
+    connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
 
     QDockWidget *ensembleNotes  = new QDockWidget("Voici toutes les notes");
-             listeNotes = new QListWidget(ensembleNotes);
-             ensembleNotes->setWidget(listeNotes);
+    ensembleNotes->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
 
-             ensembleNotes->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+    listeArticle = new QListWidget(ensembleNotes);
 
-             listeNotes = new QListWidget(ensembleNotes);
+    listeTaches = new QListWidget(ensembleNotes);
 
-             listeTaches = new QListWidget(ensembleNotes);
+    listeMulti = new QListWidget(ensembleNotes);
 
-             QWidget *ensNotes = new QWidget;
-             QLabel *labelN = new QLabel("Notes :");
-             QLabel *labelT = new QLabel("Taches :");
-             QVBoxLayout *ensembleNotesLayout = new QVBoxLayout;
-             ensembleNotesLayout->addWidget(labelN);
-             ensembleNotesLayout->addWidget(listeNotes);
-             ensembleNotesLayout->addWidget(labelT);
-             ensembleNotesLayout->addWidget(listeTaches);
+    listeArchives = new QListWidget(ensembleNotes);
 
-             ensNotes->setLayout(ensembleNotesLayout);
-             ensembleNotes->setWidget(ensNotes);
-    
-             QObject::connect(listeNotes, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(afficherArticle()));
+    QWidget *ensNotes = new QWidget;
+    QLabel *labelN = new QLabel("Articles :");
+    QLabel *labelM = new QLabel("Multimedia :");
+    QLabel *labelT = new QLabel("Taches :");
+    QLabel *labelA = new QLabel("Archives :");
+    QVBoxLayout *ensembleNotesLayout = new QVBoxLayout;
+    ensembleNotesLayout->addWidget(labelN);
+    ensembleNotesLayout->addWidget(listeArticle);
+    ensembleNotesLayout->addWidget(labelM);
+    ensembleNotesLayout->addWidget(listeMulti);
+    ensembleNotesLayout->addWidget(labelT);
+    ensembleNotesLayout->addWidget(listeTaches);
+    ensembleNotesLayout->addWidget(labelA);
+    ensembleNotesLayout->addWidget(listeArchives);
 
-             addDockWidget(Qt::LeftDockWidgetArea, ensembleNotes);
+    ensNotes->setLayout(ensembleNotesLayout);
+    ensembleNotes->setWidget(ensNotes);
 
 
-             histoNote = new QDockWidget("Voici toutes les versions de cette note");
-             histoNote->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-             addDockWidget(Qt::RightDockWidgetArea, histoNote);
+    QObject::connect(listeArticle, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(afficherArticle()));
+    QObject::connect(listeMulti, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(afficherMulti()));
+    QObject::connect(listeTaches, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(afficherTache()));
+    QObject::connect(listeArchives, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(afficherArchive()));
+
+
+
+
+    addDockWidget(Qt::LeftDockWidgetArea, ensembleNotes);
+
+
+
+    histoNote = new QDockWidget("Voici toutes les versions de cette note");
+    histoNote->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+    addDockWidget(Qt::RightDockWidgetArea, histoNote);
 
 
 
@@ -108,13 +110,9 @@ void FenetrePrincipale::afficherCreerRelation() {
 
 
 void FenetrePrincipale::afficherArticle(){
-    /*FenetreCreerTache *fenetreCreerTache = new FenetreCreerTache(this);
-    this->setCentralWidget(fenetreCreerTache);
-    fenetreCreerTache->show();*/
-
     HistoNoteManager& m=HistoNoteManager::getInstance();
-    int indice = listeNotes->currentRow();
-    HistoNotes<Article>* h=m.getHistoArticle(notes_id[indice]);
+    int indice = listeArticle->currentRow();
+    HistoNotes<Article>* h=m.getHistoArticle(articles_id[indice]);
 
 
     ArticleEditeur *fenetre= new ArticleEditeur(*(h->getLastVersion()),this);
@@ -122,35 +120,64 @@ void FenetrePrincipale::afficherArticle(){
     fenetre->show();
 };
 
-
 void FenetrePrincipale::afficherTache(){
+    HistoNoteManager& m=HistoNoteManager::getInstance();
+    int indice = listeTaches->currentRow();
+    HistoNotes<Tache>* h=m.getHistoTache(taches_id[indice]);
+
+
+    TacheEditeur *fenetre= new TacheEditeur(*(h->getLastVersion()),this);
+    this->setCentralWidget(fenetre);
+    fenetre->show();
+};
+
+void FenetrePrincipale::afficherMulti(){
+    HistoNoteManager& m=HistoNoteManager::getInstance();
+    int indice = listeMulti->currentRow();
+    HistoNotes<Multimedia>* h=m.getHistoMulti(multi_id[indice]);
+
+
+    MultiEditeur *fenetre= new MultiEditeur(*(h->getLastVersion()),this);
+    this->setCentralWidget(fenetre);
+    fenetre->show();
 
 };
 
-void FenetrePrincipale::afficherEditerMulti() {    // add 13/06
+void FenetrePrincipale::afficherArchive(){
 
 }
 
-
-void FenetrePrincipale::updateNotes() {
-    listeNotes->clear();
+void FenetrePrincipale::updateArticles() {
+    listeArticle->clear();
+    articles_id.clear();
     HistoNoteManager& m=HistoNoteManager::getInstance();
     for(HistoNoteManager::iterator<Article> it=m.begin_article(); it!=m.end_article();++it){
-        listeNotes->addItem(it.getCurrent()->getLastVersion()->getTitre());
-        notes_id.append(it.getCurrent()->getId());
+        listeArticle->addItem(it.getCurrent()->getLastVersion()->getTitre());
+        articles_id.append(it.getCurrent()->getId());
     }
+
+}
+
+void FenetrePrincipale::updateMulti(){
+    listeMulti->clear();
+    multi_id.clear();
+    HistoNoteManager& m=HistoNoteManager::getInstance();
     for(HistoNoteManager::iterator<Multimedia> it=m.begin_multi(); it!=m.end_multi();++it){
-        listeNotes->addItem(it.getCurrent()->getLastVersion()->getTitre());
-        notes_id.append(it.getCurrent()->getId());
+        listeMulti->addItem(it.getCurrent()->getLastVersion()->getTitre());
+        multi_id.append(it.getCurrent()->getId());
     }
 }
 
 void FenetrePrincipale::updateTaches(){
     listeTaches->clear();
+    taches_id.clear();
     HistoNoteManager& m=HistoNoteManager::getInstance();
     for(HistoNoteManager::iterator<Tache> it=m.begin_tache(); it!=m.end_tache();++it){
         listeTaches->addItem(it.getCurrent()->getLastVersion()->getTitre());
         taches_id.append(it.getCurrent()->getId());
     }
-};
+}
 
+void FenetrePrincipale::updateArchives(){
+
+}
