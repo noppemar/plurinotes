a0ad1a53b533a0618f3eb3ee6864999897a7d2ee
noppemar <marie.d.noppe@gmail.com>
2017-06-15 17:08:53 +0200

    Update fenprincipale.h

--- fenprincipale.h
+++ fenprincipale.h
@@ -3,28 +3,86 @@
 
 #include <QtWidgets>
 
+/*!
+ * \brief The FenetrePrincipale class
+ */
 class FenetrePrincipale : public QMainWindow
 {
     Q_OBJECT
     public:
+        /*!
+         * \brief FenetrePrincipale
+         */
         FenetrePrincipale();
+        /*!
+         * \brief updateArticles
+         * gere affichage liste article
+         */
         void updateArticles();
+        /*!
+         * \brief updateMulti
+         * gere affichage liste multi
+         */
         void updateMulti();
+        /*!
+         * \brief updateTache
+         * gere affichage liste tache
+         */
         void updateTaches();
+        /*!
+         * \brief updateArchives
+         * gere affichage liste archive
+         */
         void updateArchives();
+        /*!
+         * \brief updateRelation
+         * gere affichage liste relation
+         */
         void updateRelation();
         
     public slots:
+        /*!
+         * \brief afficherCreerArticle
+         * ouvre fenetre pour creer un article
+         */
         void afficherCreerArticle();
+        /*!
+         * \brief afficherCreerTache
+         * ouvre fenetre pour creer un tache
+         */
         void afficherCreerTache();
-        void afficherCreerMulti();   // add 13/06
+        /*!
+         * \brief afficherCreerMulti
+         * ouvre fenetre pour creer un multimedia
+         */
+        void afficherCreerMulti();
+        /*!
+         * \brief afficherCreerRelation
+         * ouvre fenetre pour creer une relation
+         */
         void afficherCreerRelation();
 
+        /*!
+         * \brief afficherArticle
+         * affiche article selectionné au milieu
+         */
         void afficherArticle();
+        /*!
+         * \brief afficherTache
+         * affiche tache selectionné au milieu
+         */
         void afficherTache();
+        /*!
+         * \brief afficherMulti
+         * affiche multimedia selectionné au milieu
+         */
         void afficherMulti();
+        /*!
+         * \brief afficherArchive
+         * affiche archive selectionné au milieu
+         */
         void afficherArchive();
-        //void afficherRelation();
+
 
     private:
 
