e01b01ff0cb8d85bde17bfcd3ea49c9dacb0d6de
noppemar <marie.d.noppe@gmail.com>
2017-06-15 17:10:40 +0200

    Update noteediteur.h

--- noteediteur.h
+++ noteediteur.h
@@ -15,7 +15,10 @@
 
 
 
-
+/*!
+ * \brief The NoteEditeur class
+ * permet afficher partie note d'un article, tache ou multi
+ */
 class NoteEditeur : public QWidget
 {
     Q_OBJECT
@@ -48,6 +51,11 @@ class NoteEditeur : public QWidget
     //gérer affichage date création, date modif
 
 public:
+    /*!
+    * \brief NoteEditeur
+    * \param n note a afficher
+    * \param p FenetrePrincipale dans laquelle afficher
+    */
    NoteEditeur(Notes& n, FenetrePrincipale *p);
    NoteEditeur(FenetrePrincipale *p);
    QVBoxLayout* getCouche(){return couche;}
@@ -64,7 +72,10 @@ public slots:
 
 
 };
-
+/*!
+ * \brief The ArticleEditeur class
+ * permet afficher Article, herite de NoteEditeur
+ */
 class ArticleEditeur : public NoteEditeur
 {
     Q_OBJECT
@@ -85,17 +96,44 @@ class ArticleEditeur : public NoteEditeur
     Article* article;
 
 public:
+    /*!
+     * \brief ArticleEditeur
+     * \param art article à afficher
+     * \param p FenetrePrincipale dans laquelle afficher
+     */
     ArticleEditeur(Article& art, FenetrePrincipale *p);
+    /*!
+     * \brief ArticleEditeur
+     * \param art article à afficher
+     * \param p FenetrePrincipale dans laquelle afficher
+     * \param j pour savoir si archive ou pas
+     */
     ArticleEditeur(Article& art, FenetrePrincipale *p,int j);
 
 signals:
 
 public slots:
+    /*!
+     * \brief saveArticle
+     * enregistrer modif article
+     */
     void saveArticle();
+    /*!
+     * \brief archiverArticle
+     * archiver un article
+     */
     void archiverArticle();
+    /*!
+     * \brief restaurerArticle
+     * restaurer un article
+     */
     void restaurerArticle();
 };
 
+/*!
+ * \brief The TacheEditeur class
+ * permet afficher Tache, herite de NoteEditeur
+ */
 class TacheEditeur : public NoteEditeur
 {
     Q_OBJECT
@@ -131,17 +169,47 @@ class TacheEditeur : public NoteEditeur
 
 
 public:
+    /*!
+     * \brief TacheEditeur
+     * \param ta tache à afficher
+     * \param p FenetrePrincipale dans laquelle afficher
+     */
     TacheEditeur(Tache& ta, FenetrePrincipale *p);
+    /*!
+     * \brief TacheEditeur
+     * \param ta tache à afficher
+     * \param p FenetrePrincipale dans laquelle afficher
+     * \param j pour savoir si archive ou pas
+     */
     TacheEditeur(Tache& ta, FenetrePrincipale *p, int j);
 
 signals:
 
 public slots:
+    /*!
+     * \brief saveTache
+     * enregistrer modif tache
+     */
     void saveTache();
+
+    /*!
+     * \brief archiverTache
+     * archiver une tache
+     */
+
     void archiverTache();
+
+    /*!
+     * \brief restaurerTache
+     * restaurer une tache
+     */
     void restaurerTache();
 };
 
+/*!
+ * \brief The MultiEditeur class
+ * permet afficher Multimedia, herite NoteEditeur
+ */
 class MultiEditeur : public NoteEditeur
 {
     Q_OBJECT
@@ -171,13 +239,36 @@ class MultiEditeur : public NoteEditeur
     Multimedia* multimedia;
 
 public:
+    /*!
+     * \brief MultiEditeur
+     * \param multi multimedia à afficher
+     * \param p FenetrePrincipale dans laquelle afficher
+     */
     MultiEditeur(Multimedia& multi, FenetrePrincipale *p);
+    /*!
+     * \brief MultiEditeur
+     * \param multi multimedia à afficher
+     * \param p FenetrePrincipale dans laquelle afficher
+     * \param j pour savoir si archive
+     */
     MultiEditeur(Multimedia& multi, FenetrePrincipale *p, int j);
 signals:
 
 public slots:
+    /*!
+     * \brief saveMulti
+     * enregistrer modif multimedia
+     */
     void saveMulti();
+    /*!
+     * \brief archiverMulti
+     * archiver un multimedia
+     */
     void archiverMulti();
+    /*!
+     * \brief restaurerMulti
+     * restaurer un multimedia
+     */
     void restaurerMulti();
 };
 
